ba2541a375c9452e571f49084e3d57bb6c6092c8
zzz <132324908+zzz0121@users.noreply.github.com>
2024-07-14 11:53:55 +0800

    Create user_config.py
    定义脚本配置

---
+++ user_config.py
@@ -0,0 +1,34 @@
+open_update = True
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "CCTV-1",
+    "CCTV-2",
+    "CCTV-3",
+    "CCTV-5",
+    "CCTV-5+",
+    "CCTV-13",
+    "浙江卫视",
+    "湖南卫视",
+]
+open_online_search = True
+favorite_page_num = 5
+default_page_num = 3
+urls_limit = 15
+open_sort = True
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = []
+open_subscribe = True
+subscribe_urls = [
+    "https://m3u.ibert.me/txt/fmml_dv6.txt",
+    "https://m3u.ibert.me/txt/o_cn.txt",
+    "https://m3u.ibert.me/txt/j_iptv.txt",
+]
+open_multicast = True
+region_list = ["all"]
+open_proxy = True
+open_driver = False
