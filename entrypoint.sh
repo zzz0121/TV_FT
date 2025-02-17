c03d22833cdb177a8493e13f34f671c3e9e99686
guorong.zheng <360996299@qq.com>
2025-02-17 15:23:19 +0800

    chore:crontab delete history

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,11 @@ done
 
 . /.venv/bin/activate
 
-current_crontab=$(crontab -l)
+crontab -d
 
 for cron_value in "$UPDATE_CRON1" "$UPDATE_CRON2"; do
-  cron_command="$cron_value cd $APP_WORKDIR && /.venv/bin/python main.py"
-  if [ -n "$cron_value" ] && ! echo "$current_crontab" | grep -q "$cron_command"; then
-    (crontab -l ; echo "$cron_command") | crontab -
+  if [ -n "$cron_value" ]; then
+    (crontab -l ; echo "$cron_value cd $APP_WORKDIR && /.venv/bin/python main.py") | crontab -
   fi
 done
 
