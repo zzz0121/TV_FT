b158c496da2ef4f22548d254d37f97331ddda4d9
guorong.zheng <360996299@qq.com>
2025-03-12 09:55:33 +0800

    fix: dir create

--- utils/tools.py
+++ utils/tools.py
@@ -26,8 +26,8 @@ def get_logger(path, level=logging.ERROR, init=False):
     """
     get the logger
     """
-    if not os.path.exists(constants.output_path):
-        os.makedirs(constants.output_path)
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    os.makedirs(os.path.dirname(constants.output_path), exist_ok=True)
     if init and os.path.exists(path):
         os.remove(path)
     handler = RotatingFileHandler(path, encoding="utf-8")
