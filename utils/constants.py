e4821ec7aa1f5c7fa5947f2df6c048af1f5001c7
Govin <37107669+Guovin@users.noreply.github.com>
2025-05-01 21:57:45 +0800

    fix: ipv6 params match

--- utils/constants.py
+++ utils/constants.py
@@ -49,7 +49,7 @@ log_path = os.path.join(output_dir, "log/log.log")
 
 url_host_pattern = re.compile(r"((https?|rtmp|rtsp)://)?([^:@/]+(:[^:@/]*)?@)?(\[[0-9a-fA-F:]+]|([\w-]+\.)+[\w-]+)")
 
-url_pattern = re.compile(r"(?P<url>" + url_host_pattern.pattern + r"(.*)?)")
+url_pattern = re.compile(r"(?P<url>" + url_host_pattern.pattern + r"[^\s]*)")
 
 rt_url_pattern = re.compile(r"^(rtmp|rtsp)://.*$")
 
