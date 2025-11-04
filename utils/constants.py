fa7b9736942b950fa020bb2584d718df2eb0a2ae
Govin <360996299@qq.com>
2025-11-04 17:09:34 +0800

    feat: sub_pattern add 4K

--- utils/constants.py
+++ utils/constants.py
@@ -79,7 +79,7 @@ multiline_m3u_pattern = re.compile(
 key_value_pattern = re.compile(r'(?P<key>\w+)=(?P<value>\S+)')
 
 sub_pattern = re.compile(
-    r"-|_|\((.*?)\)|（(.*?)）|\[(.*?)]|「(.*?)」| |｜|频道|普清|标清|高清|HD|hd|超清|超高|超高清|中央|央视|电视台|台|电信|联通|移动")
+    r"-|_|\((.*?)\)|（(.*?)）|\[(.*?)]|「(.*?)」| |｜|频道|普清|标清|高清|HD|hd|超清|超高|超高清|4K|4k|中央|央视|电视台|台|电信|联通|移动")
 
 replace_dict = {
     "plus": "+",
